67e1ea15bb9c94bd2304c5d73bfabd34d968cca1
Murat Simsek <muratsimseekk@gmail.com>
2023-12-08 21:28:37 +0300

    setup fontAwesome and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      darkBg: "rgba(37, 43, 66, 1)",
+      lightText: "rgba(255, 255, 255, 1)",
+      textColor: "rgba(37, 43, 66, 1)",
+      secondText: "rgba(115, 115, 115, 1)",
+      primaryColor: "rgba(35, 166, 240, 1)",
+      hoverText: "rgba(42, 124, 199, 1)",
+      cyanBg: "rgba(45, 139, 192, 0.75)",
+      mutedColor: "rgba(189, 189, 189, 1)",
+      secondaryColorGreen: "rgba(35, 133, 109, 1)",
+      yellow: "rgba(255, 206, 49, 1)",
+      alertColor: "rgba(231, 124, 64, 1)",
+      violet: "rgba(151, 125, 244, 1)",
+    },
     extend: {},
   },
   plugins: [],
