475a08bf18437defaf35fdadfcc529ac716aeac9
Murat Simsek <muratsimseekk@gmail.com>
2023-12-09 04:07:50 +0300

    Let the carousel navigation bar change

--- src/PageContents/CarouselWithContent.jsx
+++ src/PageContents/CarouselWithContent.jsx
@@ -4,7 +4,23 @@ import hero1 from "../assets/hero/hero1.jpeg";
 import hero2 from "../assets/hero/hero2.jpg";
 export function CarouselWithContent() {
   return (
-    <Carousel transition={{ duration: 1 }} className="h-[82vh]">
+    <Carousel
+      transition={{ duration: 1 }}
+      className="h-[82vh]"
+      navigation={({ setActiveIndex, activeIndex, length }) => (
+        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+          {new Array(length).fill("").map((_, i) => (
+            <span
+              key={i}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+              }`}
+              onClick={() => setActiveIndex(i)}
+            />
+          ))}
+        </div>
+      )}
+    >
       <div className="relative h-full w-full">
         <img src={hero1} className="h-full w-full object-fit-cover " />
         <div className="absolute  inset-0 grid h-full w-full place-items-center ">
