b7b73325b96ba7f7aabd6675fadfb5f9a6572548
Murat Simsek <muratsimseekk@gmail.com>
2023-12-10 01:50:29 +0300

    creates bestseller products section of home page

--- src/PageContents/Home.jsx
+++ src/PageContents/Home.jsx
@@ -2,12 +2,14 @@ import { Carousel } from "@material-tailwind/react";
 import React from "react";
 import { CarouselWithContent } from "./Home/CarouselWithContent";
 import EditorPicks from "./Home/EditorPicks";
+import BestSellerProducts from "./Home/BestSellerProducts";
 
 function Home() {
   return (
     <>
       <CarouselWithContent />
       <EditorPicks />
+      <BestSellerProducts />
     </>
   );
 }
---
+++ src/PageContents/Home/BestSellerProducts.jsx
@@ -0,0 +1,168 @@
+import React from "react";
+import foto1 from "../../assets/hero/bestSellerProducts/product1.png";
+import foto2 from "../../assets/hero/bestSellerProducts/product2.png";
+import foto3 from "../../assets/hero/bestSellerProducts/product3.png";
+import foto4 from "../../assets/hero/bestSellerProducts/product4.png";
+import foto5 from "../../assets/hero/bestSellerProducts/product5.png";
+import foto6 from "../../assets/hero/bestSellerProducts/product6.png";
+import foto7 from "../../assets/hero/bestSellerProducts/product7.png";
+import foto8 from "../../assets/hero/bestSellerProducts/product8.png";
+function BestSellerProducts() {
+  return (
+    <div className=" w-full py-16  flex justify-center items-center">
+      <div className=" w-4/5 h-4/5 flex flex-col gap-8">
+        <div className="text-center  flex flex-col gap-4">
+          <p className="text-secondText text-xl tracking-wider">
+            Featured Products
+          </p>
+          <h2 className="text-textColor font-bold text-2xl tracking-widest">
+            BESTSELLER PRODUCTS
+          </h2>
+          <p className="text-secondText tracking-wider">
+            Problems trying to resolve the conflict between{" "}
+          </p>
+        </div>
+        <div className=" flex justify-evenly gap-x-2 gap-y-20 flex-wrap">
+          <div className=" w-1/5 flex flex-col text-center gap-4 ">
+            <img src={foto1} />
+            <h4 className="text-textColor font-bold tracking-wider">
+              Graphic Design
+            </h4>
+            <p className="text-secondText font-medium">English Department</p>
+            <div className="flex gap-5 justify-center">
+              <span className="text-mutedColor">$16.48</span>
+              <span className="text-secondaryColorGreen font-bold">$6.48</span>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+              <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+              <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+              <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+            </div>
+          </div>
+          <div className=" w-1/5 flex flex-col text-center gap-4 ">
+            <img src={foto2} />
+            <h4 className="text-textColor font-bold tracking-wider">
+              Graphic Design
+            </h4>
+            <p className="text-secondText font-medium">English Department</p>
+            <div className="flex gap-5 justify-center">
+              <span className="text-mutedColor">$16.48</span>
+              <span className="text-secondaryColorGreen font-bold">$6.48</span>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+              <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+              <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+              <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+            </div>
+          </div>
+          <div className=" w-1/5 flex flex-col text-center gap-4 ">
+            <img src={foto3} />
+            <h4 className="text-textColor font-bold tracking-wider">
+              Graphic Design
+            </h4>
+            <p className="text-secondText font-medium">English Department</p>
+            <div className="flex gap-5 justify-center">
+              <span className="text-mutedColor">$16.48</span>
+              <span className="text-secondaryColorGreen font-bold">$6.48</span>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+              <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+              <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+              <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+            </div>
+          </div>
+          <div className=" w-1/5 flex flex-col text-center gap-4 ">
+            <img src={foto4} />
+            <h4 className="text-textColor font-bold tracking-wider">
+              Graphic Design
+            </h4>
+            <p className="text-secondText font-medium">English Department</p>
+            <div className="flex gap-5 justify-center">
+              <span className="text-mutedColor">$16.48</span>
+              <span className="text-secondaryColorGreen font-bold">$6.48</span>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+              <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+              <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+              <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+            </div>
+          </div>
+          <div className=" w-1/5 flex flex-col text-center gap-4 ">
+            <img src={foto5} />
+            <h4 className="text-textColor font-bold tracking-wider">
+              Graphic Design
+            </h4>
+            <p className="text-secondText font-medium">English Department</p>
+            <div className="flex gap-5 justify-center">
+              <span className="text-mutedColor">$16.48</span>
+              <span className="text-secondaryColorGreen font-bold">$6.48</span>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+              <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+              <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+              <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+            </div>
+          </div>
+          <div className=" w-1/5 flex flex-col text-center gap-4 ">
+            <img src={foto6} />
+            <h4 className="text-textColor font-bold tracking-wider">
+              Graphic Design
+            </h4>
+            <p className="text-secondText font-medium">English Department</p>
+            <div className="flex gap-5 justify-center">
+              <span className="text-mutedColor">$16.48</span>
+              <span className="text-secondaryColorGreen font-bold">$6.48</span>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+              <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+              <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+              <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+            </div>
+          </div>
+          <div className=" w-1/5 flex flex-col text-center gap-4 ">
+            <img src={foto7} />
+            <h4 className="text-textColor font-bold tracking-wider">
+              Graphic Design
+            </h4>
+            <p className="text-secondText font-medium">English Department</p>
+            <div className="flex gap-5 justify-center">
+              <span className="text-mutedColor">$16.48</span>
+              <span className="text-secondaryColorGreen font-bold">$6.48</span>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+              <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+              <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+              <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+            </div>
+          </div>
+          <div className=" w-1/5 flex flex-col text-center gap-4 ">
+            <img src={foto8} />
+            <h4 className="text-textColor font-bold tracking-wider">
+              Graphic Design
+            </h4>
+            <p className="text-secondText font-medium">English Department</p>
+            <div className="flex gap-5 justify-center">
+              <span className="text-mutedColor">$16.48</span>
+              <span className="text-secondaryColorGreen font-bold">$6.48</span>
+            </div>
+            <div className="flex gap-2 justify-center">
+              <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+              <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+              <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+              <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BestSellerProducts;
