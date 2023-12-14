b19ae76f5ed3d9a02c591fae8aac2602b53dc99f
Murat Simsek <muratsimseekk@gmail.com>
2023-12-14 03:10:05 +0300

    makes bottom carousel of home page responsive

--- src/PageComponents/Home/CarouselBottom.jsx
+++ src/PageComponents/Home/CarouselBottom.jsx
@@ -8,7 +8,7 @@ function CarouselBottom() {
   return (
     <Carousel
       transition={{ duration: 1 }}
-      className="h-[85vh] bg-secondaryColorGreen"
+      className="xl:h-[85vh] h-[135vh] bg-secondaryColorGreen"
       navigation={({ setActiveIndex, activeIndex, length }) => (
         <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
           {new Array(length).fill("").map((_, i) => (
@@ -24,37 +24,49 @@ function CarouselBottom() {
       )}
     >
       <div className="relative h-full w-full">
-        <img src={carousel1} className="absolute right-[10%] bottom-0 " />
-        <div className="absolute  inset-0 grid h-full w-full place-items-center ">
-          <div className="w-full  flex flex-col gap-10 ml-8 text-start md:w-3/4">
+        <img
+          src={carousel1}
+          className="absolute xl:right-[10%] xl:bottom-0 bottom-0 right-0 xl:h-4/5  xl:w-[31.5%] w-full"
+        />
+        <div className="absolute  inset-0 grid h-full w-full xl:place-items-center ">
+          <div className="w-full flex absolute top-[215px] flex-col gap-10 xl:gap-10 xl:ml-8 xl:text-start text-center md:w-3/4">
             <Typography
               variant="lead"
               color="white"
-              className="uppercase text-lightText text-xl leading-normal font-extrabold opacity-80"
+              className="uppercase text-lightText text-xl xl:leading-normal xl:font-extrabold font-medium xl:tracking-normal tracking-wider xl:opacity-80"
             >
               Summer 2023
             </Typography>
             <Typography
               variant="h1"
               color="white"
-              className=" text-lightText capitalize lg:text-6xl tracking-widest flex flex-col gap-6"
+              className=" text-lightText capitalize xl:font-bold font-black xl:text-6xl text-4xl tracking-normal  xl:tracking-widest flex flex-col xl:gap-6"
             >
               <p>vita classic</p>
-              <p>products</p>
+              <p>product</p>
             </Typography>
             <Typography
               variant="lead"
               color="white"
               className="tracking-wide text-lightText text-xl"
             >
-              We know how large objects will act, We know <br /> how are objects
-              will act, We know
+              <p className="xl:block hidden">
+                {" "}
+                We know how large objects will act, We know <br /> how are
+                objects will act, We know
+              </p>
+              <p className="xl:hidden block text-white font-medium tracking-wider">
+                We know how large objects <br /> will act, but things on a{" "}
+                <br /> small scale.
+              </p>
             </Typography>
-            <div className="flex items-center gap-10">
-              <p className="text-2xl text-lightText font-bold">$16.48</p>
+            <div className="xl:flex xl:flex-row xl:items-center flex flex-col items-center gap-6 xl:gap-10">
+              <p className="xl:text-2xl text-xl text-lightText font-semibold xl:font-bold">
+                $16.48
+              </p>
               <Button
-                size="lg"
-                className="bg-successColor text-lightText  tracking-widest rounded-lg hover:cursor-pointer px-12 text-lg uppercase"
+                size="md"
+                className="bg-successColor w-[40%] font-semibold tracking-wide text-sm xl:w-1/5  text-lightText  xl:tracking-widest rounded-md hover:cursor-pointer xl:text-base xl:font-semibold uppercase"
               >
                 add to cart
               </Button>
