f4d7f3e7b9de38b94caa7f9d3ebd543d162912f4
Murat Simsek <muratsimseekk@gmail.com>
2023-12-14 21:40:06 +0300

    Completes About section responsive design

--- src/PageComponents/About/WorkWithUs.jsx
+++ src/PageComponents/About/WorkWithUs.jsx
@@ -5,23 +5,34 @@ function WorkWithUs() {
   return (
     <div className="w-full flex justify-center">
       <div className="flex w-[95%]">
-        <div className="w-3/5  bg-primaryColor flex justify-center items-center">
-          <div className="w-2/3  flex flex-col gap-12 text-lightText">
-            <h2 className="font-semibold">WORK WITH US</h2>
-            <h3 className="font-bold tracking-wider text-4xl">
+        <div className="xl:w-3/5 w-full  bg-hoverText flex justify-center items-center">
+          <div className="w-2/3 gap-8 py-24 flex flex-col xl:items-start items-center xl:gap-12 text-lightText">
+            <h2 className="font-semibold xl:text-start text-lg xl:text-base text-center">
+              WORK WITH US
+            </h2>
+            <h3 className="font-bold xl:block hidden tracking-wider text-4xl">
               Now Let’s grow Yours
             </h3>
-            <p>
+            <h3 className="font-bold xl:hidden block text-center tracking-wider text-5xl">
+              Now Let’s <br /> grow Yours
+            </h3>
+            <p className="xl:block hidden">
               The gradual accumulation of information about atomic and <br />
               small-scale behavior during the first quarter of the 20th{" "}
             </p>
-            <Button className="capitalize tracking-wider text-sm  py-4 w-1/4 text-center bg-primaryColor border border-white">
+            <p className="xl:hidden block text-center">
+              The gradual accumulation of <br /> information about atomic and{" "}
+              <br />
+              small-scale behavior during the
+              <br /> first quarter of the 20th{" "}
+            </p>
+            <Button className="capitalize tracking-wider text-sm  py-4 w-2/5 xl:w-1/4 text-center bg-hoverText border  xl:border-white">
               Button
             </Button>
           </div>
         </div>
-        <div className="w-2/5 ">
-          <img className="w-full" src={girl} />
+        <div className="w-2/5 xl:block hidden">
+          <img className="w-full " src={girl} />
         </div>
       </div>
     </div>
