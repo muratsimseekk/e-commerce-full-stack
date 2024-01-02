2ebd9250338261cee54cda86d5dea62de2d9eab9
Murat Simsek <muratsimseekk@gmail.com>
2024-01-02 14:14:53 +0300

    Creates related products on product detailed page according to product`s category id

--- src/PageComponents/Shop/ProductPage/BestsellerProducts.jsx
+++ src/PageComponents/Shop/ProductPage/BestsellerProducts.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 
-import bestProduct1 from "../../../assets/hero/productCard/bestProduct1.png";
-import bestProduct2 from "../../../assets/hero/productCard/bestProduct2.png";
-import bestProduct3 from "../../../assets/hero/productCard/bestProduct3.png";
-import bestProduct4 from "../../../assets/hero/productCard/bestProduct4.png";
-import bestProduct5 from "../../../assets/hero/productCard/bestProduct5.png";
-import bestProduct6 from "../../../assets/hero/productCard/bestProduct6.png";
-import bestProduct7 from "../../../assets/hero/productCard/bestProduct7.png";
-import bestProduct8 from "../../../assets/hero/productCard/bestProduct8.png";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function BestsellerProducts() {
+  const param = useParams();
+
+  const paramID = param["*"].split("/")[0];
+
+  const products = useSelector((state) => state.product.productList);
+
+  const pageProduct = products.filter((item) => item.id == paramID)[0];
+
+  const productList = products
+    .filter((item) => item.category_id == pageProduct.category_id)
+    .slice(0, 12);
+  // <Link to={`/shop/category/${item.id}/${item.name}`}></Link>;
+
   return (
     <div className="py-12  w-full flex bg-lightGray justify-center">
       <div className="w-4/5 flex flex-col gap-10">
@@ -19,126 +25,28 @@ function BestsellerProducts() {
         </h2>
         <div className="w-full h-1 bg-gray-200"></div>
         <div className="flex flex-col gap-14  xl:shadow-none xl:gap-x-0 xl:flex xl:flex-row xl:flex-wrap xl:justify-between xl:gap-y-12 ">
-          <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
-            <img className="w-full" src={bestProduct1} />
-            <div className="flex flex-col gap-6 items-start pl-6 ">
-              <h4 className="text-textColor font-bold tracking-wider">
-                Graphic Design
-              </h4>
-              <p className="text-secondText font-medium">English Department</p>
-              <div className="flex gap-5 justify-center">
-                <span className="text-mutedColor">$16.48</span>
-                <span className="text-secondaryColorGreen font-bold">
-                  $6.48
-                </span>
+          {productList.map((item) => {
+            return (
+              <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
+                <Link to={`/shop/category/${item.id}/${item.name}`}>
+                  <img className="w-full" src={item.images[0].url} />
+                </Link>
+                <div className="flex flex-col gap-6 items-start pl-6 ">
+                  <h4 className="text-textColor font-bold tracking-wider">
+                    {item.name}
+                  </h4>
+                  <p className="text-secondText text-start font-medium">
+                    {item.description}
+                  </p>
+                  <div className="flex gap-5 justify-center">
+                    <span className="text-secondaryColorGreen font-bold">
+                      $ {item.price}
+                    </span>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-          <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
-            <img className="w-full" src={bestProduct2} />
-            <div className="flex flex-col gap-6 items-start pl-6 ">
-              <h4 className="text-textColor font-bold tracking-wider">
-                Graphic Design
-              </h4>
-              <p className="text-secondText font-medium">English Department</p>
-              <div className="flex gap-5 justify-center">
-                <span className="text-mutedColor">$16.48</span>
-                <span className="text-secondaryColorGreen font-bold">
-                  $6.48
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
-            <img className="w-full" src={bestProduct3} />
-            <div className="flex flex-col gap-6 items-start pl-6 ">
-              <h4 className="text-textColor font-bold tracking-wider">
-                Graphic Design
-              </h4>
-              <p className="text-secondText font-medium">English Department</p>
-              <div className="flex gap-5 justify-center">
-                <span className="text-mutedColor">$16.48</span>
-                <span className="text-secondaryColorGreen font-bold">
-                  $6.48
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
-            <img className="w-full" src={bestProduct4} />
-            <div className="flex flex-col gap-6 items-start pl-6 ">
-              <h4 className="text-textColor font-bold tracking-wider">
-                Graphic Design
-              </h4>
-              <p className="text-secondText font-medium">English Department</p>
-              <div className="flex gap-5 justify-center">
-                <span className="text-mutedColor">$16.48</span>
-                <span className="text-secondaryColorGreen font-bold">
-                  $6.48
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
-            <img className="w-full" src={bestProduct5} />
-            <div className="flex flex-col gap-6 items-start pl-6 ">
-              <h4 className="text-textColor font-bold tracking-wider">
-                Graphic Design
-              </h4>
-              <p className="text-secondText font-medium">English Department</p>
-              <div className="flex gap-5 justify-center">
-                <span className="text-mutedColor">$16.48</span>
-                <span className="text-secondaryColorGreen font-bold">
-                  $6.48
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
-            <img className="w-full" src={bestProduct6} />
-            <div className="flex flex-col gap-6 items-start pl-6 ">
-              <h4 className="text-textColor font-bold tracking-wider">
-                Graphic Design
-              </h4>
-              <p className="text-secondText font-medium">English Department</p>
-              <div className="flex gap-5 justify-center">
-                <span className="text-mutedColor">$16.48</span>
-                <span className="text-secondaryColorGreen font-bold">
-                  $6.48
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
-            <img className="w-full" src={bestProduct7} />
-            <div className="flex flex-col gap-6 items-start pl-6 ">
-              <h4 className="text-textColor font-bold tracking-wider">
-                Graphic Design
-              </h4>
-              <p className="text-secondText font-medium">English Department</p>
-              <div className="flex gap-5 justify-center">
-                <span className="text-mutedColor">$16.48</span>
-                <span className="text-secondaryColorGreen font-bold">
-                  $6.48
-                </span>
-              </div>
-            </div>
-          </div>
-          <div className=" xl:w-[23%] flex flex-col text-center gap-6 bg-lightText pb-10">
-            <img className="w-full" src={bestProduct8} />
-            <div className="flex flex-col gap-6 items-start pl-6 ">
-              <h4 className="text-textColor font-bold tracking-wider">
-                Graphic Design
-              </h4>
-              <p className="text-secondText font-medium">English Department</p>
-              <div className="flex gap-5 justify-center">
-                <span className="text-mutedColor">$16.48</span>
-                <span className="text-secondaryColorGreen font-bold">
-                  $6.48
-                </span>
-              </div>
-            </div>
-          </div>
+            );
+          })}
         </div>
       </div>
     </div>
