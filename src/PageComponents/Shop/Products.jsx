6689ed6fce3f1167317db020a13fd60a5a07cc88
Murat Simsek <muratsimseekk@gmail.com>
2024-01-04 01:18:50 +0300

    infinite scroll 25 products

--- src/PageComponents/Shop/Products.jsx
+++ src/PageComponents/Shop/Products.jsx
@@ -23,12 +23,13 @@ import { AxiosInstance } from "../../api/api";
 import { productFetch } from "../../store/actions/thunkAction";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function Products() {
   const dispatch = useDispatch();
   const categories = useSelector((state) => state.general.categories);
-  const [loading, setLoading] = useState();
-  const [searchValue, setSearchValue] = useState();
+  const [loading, setLoading] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
   const [sorted, setSorted] = useState("");
 
   const [page, setPage] = useState(0);
@@ -36,15 +37,19 @@ function Products() {
   const parametre = urlParam["*"].replace("/", ":");
 
   useEffect(() => {
-    const categoryID = categories.find((item) => item.code === parametre);
-    dispatch(productFetch({ category: categoryID?.id }));
-  }, [parametre]);
-
-  useEffect(() => {
-    const fetchingProducts = async () => {
+    const fetchData = () => {
       setLoading(true);
       try {
-        dispatch(productFetch({ offset: page * 25 }));
+        const categoryID = categories.find((item) => item.code === parametre);
+        dispatch(
+          productFetch({
+            sort: sorted,
+            filter: searchValue,
+            category: categoryID?.id,
+            offset: page * 25,
+          })
+        );
+        console.log("calisti fetch data");
       } catch (err) {
         toast.error("An error occurred while fetching products");
       } finally {
@@ -52,34 +57,31 @@ function Products() {
       }
     };
 
-    if (page > 0) {
-      fetchingProducts();
+    // Fetch data only on the initial render
+    if (page === 0) {
+      fetchData();
     }
-  }, [dispatch, page]);
-
-  useEffect(() => {
-    const handleScroll = (e) => {
-      const scrollHeight = e.target.documentElement.scrollHeight;
-      const currentHeight =
-        e.target.documentElement.scrollTop + window.innerHeight;
-      if (currentHeight + 1 >= scrollHeight) {
-        setPage((prev) => prev + 1);
-
-        dispatch(productFetch({ offset: page * 25, sort: sorted }));
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
   }, [page]);
 
-  //SORT Codes
+  const fetchMoreData = () => {
+    setPage((prevPage) => prevPage + 1);
+    dispatch(
+      productFetch({
+        sort: sorted,
+        filter: searchValue,
+        category: categoryID?.id,
+        offset: (page + 1) * 25,
+      })
+    );
+  };
+
   const sortHandler = (e) => {
     setSorted(e);
   };
 
+  const categoryID = categories.find((item) => item.code === parametre);
+
   const sendSort = () => {
-    const categoryID = categories.find((item) => item.code === parametre);
     dispatch(
       productFetch({
         sort: sorted,
@@ -99,7 +101,7 @@ function Products() {
     <div className="flex justify-center items-center  pt-8 ">
       <div className="flex flex-col w-full items-center gap-12">
         <div className="w-11/12 flex flex-col gap-10 items-center  xl:flex xl:flex-row xl:justify-between xl:items-center">
-          <h2 className=" text-secondText text-base font-medium">
+          <h2 className="text-secondText text-base font-medium">
             Showing all{" "}
             <span className="font-semibold">{products?.length}</span> results
           </h2>
@@ -128,45 +130,63 @@ function Products() {
                   sendSort();
                 }}
                 size="lg"
-                className="capitalize tracking-wider bg-primaryColor text-lightText "
+                className="capitalize tracking-wider bg-primaryColor text-lightText"
               >
                 Filter
               </Button>
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-16 xl:gap-0 xl:flex xl:flex-row w-11/12 xl:flex-wrap xl:pt-5 xl:justify-between xl:gap-y-12">
-          {products?.map((item, i) => (
-            <div
-              key={i}
-              className=" xl:w-[23%] flex flex-col text-center justify-between xl:gap-6 gap-8 "
-            >
-              <Link to={`/shop/category/${item.id}/${item.name}`}>
-                <img className="w-[100%] " src={item.images[0].url} />
-              </Link>
-              <h4 className="text-textColor font-bold tracking-wider">
-                {item.name}
-              </h4>
-              <p className="text-secondText font-normal text-sm">
-                {item.description}
-              </p>
-              <div className="flex gap-3 xl:gap-5 justify-center">
-                <span className="text-secondaryColorGreen font-bold">
-                  $ {item.price}
-                </span>
-              </div>
-              <div className="flex gap-2 justify-center">
-                <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
-                <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
-                <button className="h-5 w-5 bg-alertColor rounded-full"></button>
-                <button className="h-5 w-5 bg-darkBg rounded-full"></button>
-              </div>
+        <InfiniteScroll
+          dataLength={products?.length}
+          next={fetchMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <div className="flex justify-center">
+            <div className="flex flex-col gap-16 xl:gap-0 xl:flex xl:flex-row w-11/12 xl:flex-wrap xl:pt-5 xl:justify-between xl:gap-y-12">
+              {products?.map((item, i) => (
+                <div
+                  key={i}
+                  className=" xl:w-[23%] flex flex-col text-center justify-between xl:gap-6 gap-8 "
+                >
+                  <Link to={`/shop/category/${item.id}/${item.name}`}>
+                    <img
+                      className="w-[100%] "
+                      src={item.images[0].url}
+                      alt={item.name}
+                    />
+                  </Link>
+                  <h4 className="text-textColor font-bold tracking-wider">
+                    {item.name}
+                  </h4>
+                  <p className="text-secondText font-normal text-sm">
+                    {item.description}
+                  </p>
+                  <div className="flex gap-3 xl:gap-5 justify-center">
+                    <span className="text-secondaryColorGreen font-bold">
+                      $ {item.price}
+                    </span>
+                  </div>
+                  <div className="flex gap-2 justify-center">
+                    <button className="h-5 w-5 bg-primaryColor rounded-full"></button>
+                    <button className="h-5 w-5 bg-secondaryColorGreen rounded-full"></button>
+                    <button className="h-5 w-5 bg-alertColor rounded-full"></button>
+                    <button className="h-5 w-5 bg-darkBg rounded-full"></button>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        </InfiniteScroll>
 
         <div className="w-full  flex justify-center bg-lightGray">
-          <div className="w-[85%] py-6 pb-8 flex flex-col items-center gap-8 xl:flex xl:flex-row xl:justify-between xl:items-center  text-secondText">
+          <div className="w-[85%] py-6 pb-8 flex flex-col items-center gap-8 xl:flex xl:flex-row xl:justify-between xl:items-center text-secondText">
             <FontAwesomeIcon
               icon={faHooli}
               className="xl:w-24 xl:h-24 w-40 h-40"
