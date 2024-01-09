c2d5b34f0dcba93a051db9a4a46176f5260991d5
Murat Simsek <muratsimseekk@gmail.com>
2024-01-09 19:54:30 +0300

    styles card inputs

--- src/PageComponents/Shop/OrderCompletePage.jsx
+++ src/PageComponents/Shop/OrderCompletePage.jsx
@@ -1,5 +1,7 @@
 import { Button } from "@material-tailwind/react";
 import React, { useEffect, useState } from "react";
+import Cards from "react-credit-cards-2";
+import "react-credit-cards-2/dist/es/styles-compiled.css";
 
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
@@ -15,18 +17,14 @@ import { FaCcMastercard } from "react-icons/fa";
 import { GrRadialSelected } from "react-icons/gr";
 
 import { AxiosInstance } from "../../api/api";
-import { CountryDropdown, RegionDropdown } from "react-country-region-selector";
+
 import { set, useForm } from "react-hook-form";
 
 function OrderCompletePage() {
   const [cartProducts, setCartProducts] = useState([]);
   const [totalProduct, setTotalProduct] = useState(0);
 
-  // country and region selection
-  const [country, setCountry] = useState("");
-  const [region, setRegion] = useState("");
-
-  const [newAddressMenu, setNewAddressMenu] = useState(false);
+  const [newCard, setNewCard] = useState(false);
 
   const [totalPrice, setTotalPrice] = useState(0);
 
@@ -131,6 +129,36 @@ function OrderCompletePage() {
       navigate("/complete-order");
     }
   };
+
+  const saveCardHandler = () => {};
+
+  const [state, setState] = useState({
+    number: "",
+    expiry: "",
+    cvc: "",
+    name: "",
+    focus: "",
+  });
+  console.log("state", state);
+
+  const handleInputChange = (evt) => {
+    const { name, value } = evt.target;
+
+    if (
+      (name == "number" && value.length > 16) ||
+      (name == "expiry" && value.length > 4) ||
+      (name == "cvc" && value.length > 3)
+    ) {
+      return;
+    }
+
+    setState((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleInputFocus = (evt) => {
+    setState((prev) => ({ ...prev, focus: evt.target.name }));
+  };
+
   return (
     <div className="w-full  flex justify-center py-20">
       <div className=" w-[73%] flex flex-col gap-6">
@@ -164,9 +192,8 @@ function OrderCompletePage() {
             </p>
           </div>
         </div>
-
         <div className=" flex flex-col  rounded-md border-[#6CB9D8] border">
-          <div className="flex px-4 py-3 gap-4 items-center border rounded-md border-[#6CB9D8]">
+          <div className="flex px-4 py-3 gap-4 items-center border-b rounded-md border-[#6CB9D8]">
             <GrRadialSelected className="w-6 h-6 text-primaryColor" />
             <div>
               <h2 className="text-lg font-semibold tracking-wider">
@@ -182,13 +209,16 @@ function OrderCompletePage() {
             <div className="w-3/5 border-r-2 gap-6 px-4 py-3 flex flex-col">
               <div className="flex justify-between ">
                 <h3 className="text-base font-semibold">Kart Bilgileri</h3>
-                <p className="text-sm font-medium underline text-secondText">
+                <p
+                  onClick={() => setNewCard(true)}
+                  className="text-sm font-medium hover:cursor-pointer underline text-secondText"
+                >
                   Baska bir Kart ile Odeme yap
                 </p>
               </div>
               <div className="flex justify-between flex-wrap gap-y-6">
                 <div className="flex w-[47%] flex-col">
-                  <div className="flex items-center bg-roseBg rounded-t-lg gap-3">
+                  <div className="flex items-center  rounded-t-lg gap-3">
                     <input type="radio" className="w-4 h-4" name="" id="" />
                     <h2>BONUS kartim</h2>
                   </div>
@@ -208,7 +238,7 @@ function OrderCompletePage() {
                   </div>
                 </div>
                 <div className="flex w-[47%] flex-col">
-                  <div className="flex items-center bg-roseBg rounded-t-lg gap-3">
+                  <div className="flex items-center  rounded-t-lg gap-3">
                     <input type="radio" className="w-4 h-4" name="" id="" />
                     <h2>BONUS kartim</h2>
                   </div>
@@ -228,7 +258,7 @@ function OrderCompletePage() {
                   </div>
                 </div>
                 <div className="flex w-[47%] flex-col">
-                  <div className="flex items-center bg-roseBg rounded-t-lg gap-3">
+                  <div className="flex items-center  rounded-t-lg gap-3">
                     <input type="radio" className="w-4 h-4" name="" id="" />
                     <h2>BONUS kartim</h2>
                   </div>
@@ -249,17 +279,156 @@ function OrderCompletePage() {
                 </div>
               </div>
             </div>
-            <div className="w-2/5 pl-4">
+            <div className="w-2/5 flex flex-col gap-5 px-4">
               <div>
                 <h3 className="text-base font-semibold ">Taksit Secenekleri</h3>
                 <p className="text-sm font-medium text-secondText">
                   Kartiniza uygun taksit secenegini seciniz
                 </p>
               </div>
-              <div></div>
+              <div className="border border-[#6CB9D8] rounded-lg  flex ">
+                <div className=" w-1/2 border-r border-[#6CB9D8]">
+                  <div className="border-b border-[#6CB9D8] bg-[#EDF6FA] rounded-l-lg pl-4 py-2 tracking-wide font-medium">
+                    Taksit Sayisi
+                  </div>
+                  <div className="pl-4 py-2 text-primaryColor items-center flex gap-3 ">
+                    <input
+                      className="accent-[#6CB9D8]"
+                      type="radio"
+                      name=""
+                      id=""
+                    />
+                    <p className="text-sm ">Tek Cekim</p>
+                  </div>
+                </div>
+                <div className="w-1/2">
+                  <div className="border-b border-[#6CB9D8] bg-[#EDF6FA] rounded-r-lg pl-4 py-2 tracking-wide font-medium">
+                    Aylik Odeme
+                  </div>
+                  <div className="pl-4 py-2 text-primaryColor">6.604,22 TL</div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
+        {newCard && (
+          <div className="w-full  flex justify-center">
+            <form
+              onSubmit={handleSubmit(saveCardHandler)}
+              className="w-full border flex flex-col gap-4 border-[#6CB9D8] rounded-md px-4 py-6"
+            >
+              <div className="flex justify-between items-center pb-2 border-b-2 border-[#EDF6FA] ">
+                <h3 className="text-xl font-semibold ">Kart Bilgileri</h3>
+                <p
+                  onClick={() => setNewCard(false)}
+                  className="text-sm hover:cursor-pointer font-medium text-secondText underline"
+                >
+                  Kayitli kartimla odeme yap
+                </p>
+              </div>
+
+              <Cards
+                number={state.number}
+                expiry={state.expiry}
+                cvc={state.cvc}
+                name={state.name}
+                focused={state.focus}
+              />
+              <form className="flex flex-col flex-wrap gap-y-4 ">
+                <div className="flex gap-6 items-center">
+                  <label
+                    className=" border-b-2 border-l-2 border-l-secondText border-b-secondText text-darkBg px-2 py-1 "
+                    htmlFor="number"
+                  >
+                    Card Number :
+                  </label>
+                  <input
+                    {...register("number", {
+                      required: true,
+                      minLength: 16,
+                      maxLength: 16,
+                    })}
+                    id="number"
+                    type="number"
+                    name="number"
+                    placeholder="Card Number"
+                    value={state.number}
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                    className=" border border-l-4 border-l-darkBg  bg-[#EDF6FA] p-1 rounded-md"
+                  />
+                </div>
+                <div className="flex gap-6 items-center">
+                  <label
+                    className=" border-b-2 border-l-2 border-l-secondText border-b-secondText text-darkBg px-2 py-1 "
+                    htmlFor="name"
+                  >
+                    Card Holder Name :
+                  </label>
+                  <input
+                    id="name"
+                    type="text"
+                    name="name"
+                    placeholder="Full Name"
+                    value={state.name}
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                    className=" border border-l-4 border-l-darkBg  bg-[#EDF6FA] p-1 rounded-md"
+                  />
+                </div>
+                <div className="flex gap-6 items-center">
+                  <label
+                    className=" border-b-2 border-l-2 border-l-secondText border-b-secondText text-darkBg px-2 py-1 "
+                    htmlFor="expiry"
+                  >
+                    Expiry Date :
+                  </label>
+                  <input
+                    {...register("expiry", {
+                      required: true,
+                      minLength: 4,
+                      maxLength: 4,
+                    })}
+                    id="expiry"
+                    type="number"
+                    name="expiry"
+                    placeholder="Expiration Date"
+                    value={state.expiry}
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                    className=" border border-l-4 border-l-darkBg  bg-[#EDF6FA] p-1 rounded-md"
+                  />
+                </div>
+                <div className="flex gap-6 items-center">
+                  <label
+                    className=" border-b-2 border-l-2 border-l-secondText border-b-secondText text-darkBg px-2 py-1 "
+                    htmlFor="cvc"
+                  >
+                    CVC :
+                  </label>
+                  <input
+                    id="cvc"
+                    type="number"
+                    name="cvc"
+                    placeholder="Expiration Date"
+                    value={state.cvc}
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                    className=" border border-l-4 border-l-darkBg  bg-[#EDF6FA] p-1 rounded-md"
+                  />
+                </div>
+                <div className=" flex justify-center">
+                  <button
+                    className="bg-darkBg text-white px-2 py-1 rounded-lg hover:bg-white hover:text-darkBg border border-darkBg hover:font-medium"
+                    type="submit"
+                  >
+                    Save Card
+                  </button>
+                </div>
+              </form>
+            </form>
+          </div>
+        )}
       </div>
       <div className="w-[22%] flex flex-col items-center justify-start">
         <div className="w-5/6 py-2  flex gap-2 justify-center">
